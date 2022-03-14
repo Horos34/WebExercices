c2811a09bc633ca0cf04588a2348b73d9794b319
Pedram Aguiard <aguiardpedram@gmail.com>
2022-03-14 16:10:00 +0100

    Init refonte du site

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -6,23 +6,23 @@ function multiplicateur(value1, value2) {
 }
 
 function WeactButton() {
-  var smurk = "CAN I GET A HOOOOO YEAAAAAAAAAAH ?";
+  let smurk = "CAN I GET A HOOOOO YEAAAAAAAAAAH ?";
   alert(smurk);
 }
 
 function alertButton() {
-  var clic = "L'action a bien été prise en compte";
+  let clic = "L'action a bien été prise en compte";
   alert(clic);
 }
 
 function saisieDonnée() {
-  var saisie_prenom = document.getElementById("prenom").value;
-  var saisie_nom = document.getElementById("Nomdefamille").value;
+  let saisie_prenom = document.getElementById("prenom").value;
+  let saisie_nom = document.getElementById("Nomdefamille").value;
   alert("Tu t'appelles " + saisie_prenom + " " + saisie_nom);
 }
 
 function stringAnalyze(x, y) {
-  var parsed = parseInt(x, y);
+  let parsed = parseInt(x, y);
     if (isNaN(parsed)) {
       return 0;
     }
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # WebExercices
 
 ## Repository de base pour le WebExercices
+
+- Page perso de Weact et Horos visant à s'exercer à la fabrication d'une IU.
+> Sous réserve d'être modifié très souvent 
+
